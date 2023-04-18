0782c79a749d82fc8750b2d358a5fe4b772f4deb
Ibrahim Kural <ikural46@gmail.com>
2023-04-18 05:15:51 +0300

    a program to find even numbers Up to the entered number

--- untitledProgram to Find Even Numbers Up To The Entered Number/src/Main.java
+++ untitledProgram to Find Even Numbers Up To The Entered Number/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int k;
+        Scanner inp=new Scanner(System.in);
+        System.out.println("enter a number: ");
+        k=inp.nextInt();
+        int i=1;
+        while ( i<=k ) {
+            if ((i%3==0) && (i%4==0))
+                System.out.println(i);
+            i++;
+            }
+
+
+
     }
 }
