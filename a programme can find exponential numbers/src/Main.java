323619b9582a4df034fa60819e2469e9dd3ccd71
Ibrahim Kural <ikural46@gmail.com>
2023-04-22 00:49:10 +0300

    a programme that finds exponential numbers

--- a programme can find exponential numbers/src/Main.java
+++ a programme can find exponential numbers/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+    static void power() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban değeri giriniz :");
+        int base = scan.nextInt();
+        System.out.print("Üs değeri giriniz :");
+        int exponent = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        System.out.println("Sonuc: " ); power();
     }
 }
