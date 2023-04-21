30fa5eecfa5c38797a137ec4d1197a0d2ef43918
Ibrahim Kural <ikural46@gmail.com>
2023-04-21 08:44:34 +0300

    palindrom numbers

--- Palindrom Numbers/src/Main.java
+++ Palindrom Numbers/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static boolean isPalindrom(int number) {
+        int temp= number, reverseNumber=0, lastNumber;
+        while (temp!=0) {
+            lastNumber= temp % 10;
+            reverseNumber= (reverseNumber*10) + lastNumber;
+            temp/=10;
+        }
+        if (number==reverseNumber)
+            return true;
+        else
+            return false;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number;
+        Scanner input=new Scanner(System.in);
+        number=input.nextInt();
+        System.out.print("Bir Sayi Giriniz: ");
+        isPalindrom(number);
+
+
+
     }
 }
