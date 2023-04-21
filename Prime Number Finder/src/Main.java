bc80a87fac4f004ed79c3e7a4d3da21a80ca2df4
Ibrahim Kural <ikural46@gmail.com>
2023-04-22 01:38:33 +0300

    finding prime numbers

--- Prime Number Finder/src/Main.java
+++ Prime Number Finder/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static boolean asalMi(int sayi) {
+        int sayac = 0;
+        for(int i = 2; i < sayi; i++)
+        {
+            if(sayi % i == 0) {
+                sayac++;
+            }
+        }
+        return sayac == 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Bir sayi giriniz: ");
+        int sayi = scan.nextInt();
+
+        if(asalMi(sayi)) {
+            System.out.println("Girilen sayi asal bir sayidir.");
+        }
+        else {
+            System.out.println("Girilen sayi asal bir sayi degildir.");
+        }
+
     }
 }
