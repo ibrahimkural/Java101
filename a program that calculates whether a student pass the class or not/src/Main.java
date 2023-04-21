cd5f9096fecc958cceb54dae36bd603ee908e239
Ibrahim Kural <ikural46@gmail.com>
2023-04-21 09:07:10 +0300

    calculating whether a student pass class or not

--- a program that calculates whether a student pass the class or not/src/Main.java
+++ a program that calculates whether a student pass the class or not/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int math, physics, turkish, chemistry, music, total;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your math score");
+        math=input.nextInt();
+        System.out.println("Enter your physics score");
+        physics=input.nextInt();
+        System.out.println("Enter your turkish score");
+        turkish=input.nextInt();
+        System.out.println("Enter your chemistry score");
+        chemistry=input.nextInt();
+        System.out.println("Enter your music score");
+        music=input.nextInt();
+
+        System.out.println("Congrats you past");
+
+
     }
 }
