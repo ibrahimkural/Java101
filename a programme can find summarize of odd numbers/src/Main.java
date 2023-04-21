220bc48c996518143d153f5940bce79e8054ab85
Ibrahim Kural <ikural46@gmail.com>
2023-04-21 12:50:20 +0300

    a programme can find summarize of even numbers

--- a programme can find summarize of odd numbers/src/Main.java
+++ a programme can find summarize of odd numbers/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     int n;
+     int total=0;
+
+        Scanner input=new Scanner(System.in);
+        do {
+            System.out.println("Sayi Giriniz : ");
+            n = input.nextInt();
+            if ((n % 4 == 0)) {
+                total += n;
+            }
+        }
+            while (n % 2 != 1) ;
+
+        System.out.println("Toplam :" + total);
+
     }
 }
