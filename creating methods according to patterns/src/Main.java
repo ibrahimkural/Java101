7cc7f50f3d934a1ea8f0ef71f0bb8a10efd2d622
Ibrahim Kural <ikural46@gmail.com>
2023-04-22 22:54:21 +0300

    a programme can find the exponential numbers of two

--- creating methods according to patterns/src/Main.java
+++ creating methods according to patterns/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static int pattern(int k) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Bir sayi giriniz :");
+        int  b= scan.nextInt();
+        int result=0;
+        if (b==0) {
+            return 0;
+
+        }
+            return pattern(b-5)+b;
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        pattern(10);
     }
 }
