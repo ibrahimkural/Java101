66b355478df3dd34ba7d58624e8cac0a45d81337
Ibrahim Kural <ikural46@gmail.com>
2023-04-22 23:49:12 +0300

    exponenatial numbers of four and five

--- A programme can find the exponential numbers of two/src/Main.java
+++ A programme can find the exponential numbers of two/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inp= new Scanner(System.in);
+        int sayi;
+        System.out.println("Bir Sayi Giriniz : ");
+        sayi=inp.nextInt();
+        int i=1, k=1;
+        while ( i<=sayi) {
+            i*=4;
+            System.out.println(i);
+        }
+        while ( k<=sayi) {
+            k*=5;
+            System.out.println(k);
+        }
     }
 }
