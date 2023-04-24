f470a98cd0b34ea949dea38f1df8c38cb940e12e
Ibrahim Kural <ikural46@gmail.com>
2023-04-24 05:36:35 +0300

    calculating factorial

--- factorial calculation/src/Main.java
+++ factorial calculation/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner inp=new Scanner(System.in);
+        System.out.println("Faktoriyel Giriniz : ");
+        int n=inp.nextInt();
+        int i=1, toplam=1;
+        while (i<=n) {
+            toplam *=i;
+            i++;
+        }
+
+
+        System.out.println( n + ".Factorial :" + toplam);
     }
 }
