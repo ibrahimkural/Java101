967f6b78843a9716727529d4e6a7a10bf15a2aa2
Ibrahim Kural <ikural46@gmail.com>
2023-04-24 05:49:24 +0300

    calculating exponential numbers

--- calculating exponential numbers/src/Main.java
+++ calculating exponential numbers/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n, e, total=1;
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter a number");
+        n= input.nextInt();
+        System.out.println("enter an exponent");
+        e=input.nextInt();
+
+        for ( int i=1; i<=e; i++) {
+            total*=n;
+        }
+
+        System.out.println("Result : " + total);
     }
 }
