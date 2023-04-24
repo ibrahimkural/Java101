f5e65da94df8f1f74278f2c671675d16a19dc268
Ibrahim Kural <ikural46@gmail.com>
2023-04-24 06:29:35 +0300

    Suggesting Events Based on Air Temperature

--- Suggesting Events Based On Air Temperature/src/Main.java
+++ Suggesting Events Based On Air Temperature/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter the weather temperature");
+        int heat;
+        heat= input.nextInt();
+
+        if (heat<=5) {
+            System.out.println("You can ski");
+        }
+        else if (heat<=15) {
+            System.out.println("You can go to the cinema");
+        }
+        else if (heat<=25) {
+            System.out.println("You can have a picnic");
+        }
+        else {
+            System.out.println("You can go to swim");
+        }
+
+
     }
 }
