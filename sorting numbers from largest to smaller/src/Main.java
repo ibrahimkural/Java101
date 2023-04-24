c97d123217df588155f0ae9353adfd38789b1384
Ibrahim Kural <ikural46@gmail.com>
2023-04-24 06:52:38 +0300

    sorting numbers from largest to smaller

--- sorting numbers from largest to smaller/src/Main.java
+++ sorting numbers from largest to smaller/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a,b,c;
+        Scanner inp=new Scanner(System.in);
+        System.out.println("Enter a number");
+        a=inp.nextInt();
+        System.out.println("Enter a number");
+        b=inp.nextInt();
+        System.out.println("Enter a number");
+        c=inp.nextInt();
+        if ((a>b)&&(a>c)) {
+            if (b>c) {
+                System.out.println("a>b>c");
+
+            } else {
+                System.out.println("a>c>b");
+            }
+        } else if ((b>a)&&(b>c)) {
+            if (a>c) {
+                System.out.println("b>a>c");
+
+            } else {
+                System.out.println("b>c>a");
+            }
+        }   else  {
+            if (b>a) {
+                System.out.println("c>b>a");
+
+            } else {
+                System.out.println("c>a>b");
+            }
+        }
     }
 }
